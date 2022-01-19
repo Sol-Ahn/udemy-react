d693c6102b4a387a268e1157221ee21f50ff5a91
Sol-Ahn <sorry0194@gmail.com>
2022-01-19 23:59:26 +0900

    add: section 12

--- project-06/src/App.js
+++ project-06/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showParagraph, setShowParagraph] = useState(false);
+	const [allowToggle, setAllowToggle] = useState(false);
+
+	console.log("APP RUNNING");
+
+	const toggleParagraphHandler = useCallback(() => {
+		if (allowToggle) {
+			setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+		}
+	}, [allowToggle]);
+
+	const allowToggleHandler = () => {
+		setAllowToggle(true);
+	};
+
+	return (
+		<div className="app">
+			<h1>Hi there!</h1>
+			<DemoOutput show={showParagraph} />
+			<Button onClick={allowToggleHandler}>Allow Toggling!</Button>
+			<Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+		</div>
+	);
 }
 
 export default App;
