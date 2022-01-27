150753dc788537f1ffa10cd16c74286e6ec4c804
Sol-Ahn <sorry0194@gmail.com>
2022-01-27 17:26:39 +0900

    add: section 20

---
+++ project-14-SPA/src/App.js
@@ -0,0 +1,36 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+	return (
+		<div>
+			<MainHeader />
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to="/welcome" />
+					</Route>
+					<Route path="/welcome">
+						<Welcome />
+					</Route>
+					<Route path="/products" exact>
+						<Products />
+					</Route>
+					<Route path="/products/:productId">
+						<ProductDetail />
+					</Route>
+				</Switch>
+			</main>
+		</div>
+	);
+}
+
+export default App;
+
+// domain.com/welcome => Welcome Component
+// domain.com/products => Products Component
+// domain.com/product-detail/{productId}
